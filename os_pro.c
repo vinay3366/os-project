7322e2aef0f984ec8ca21a3d65cc73e96fea94ec
vinay3366 <49425082+vinay3366@users.noreply.github.com>
2019-04-19 18:47:04 +0530

    Update os_pro.c

--- os_pro.c
+++ os_pro.c
@@ -1,3 +1,9 @@
+*/ Two types of people can enter into a library- students and teachers. After entering the library, the visitor searches for the required
+books and gets them. In order to get them issued, he goes to the single CPU which is there to process the issuing of books. Two types of
+queues are there at the counter-one for students and one for teachers. A student goes and stands at the tail of the queue for students
+and similarly the teacher goes and stands at the tail of the queue for teachers (FIFO). If a student is being serviced and a teacher 
+arrives at the counter,he would be the next person to get service (PRIORITY-non preemptive).If two teachers arrive at the same time, 
+they will stand in their queue to get service (FIFO). WAP to ensure that the system works in a non-chaotic manner. /*
 #include<stdio.h>
 int no_studen=0;
 int no_teach=0;
