754cd93f466d5a0ce2b9832d3b3c121a8fc64ca1
vinay3366 <49425082+vinay3366@users.noreply.github.com>
2019-04-19 18:37:18 +0530

    Update Os.c

--- Os.c
+++ Os.c
@@ -1,3 +1,9 @@
+*/There are 3 student processes and 1 teacher process. Students are supposed to do their assignments and they need 3 things for that-pen,
+paper and question paper. The teacher has an infinite supply of all the three things. One students has pen, another has paper and another
+has question paper. The teacher places two things on a shared table and the student having the third complementary thing makes the 
+assignment and tells the teacher on completion. The teacher then places another two things out of the three and again the student 
+having the third thing makes the assignment and tells the teacher on completion. This cycle continues. WAP to synchronise the teacher 
+and the students. /*
 #include <stdio.h>
 #include <string.h>
 int student1=0,student2=0,student3=0 ;
